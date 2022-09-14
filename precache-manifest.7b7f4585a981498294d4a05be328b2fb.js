3039b4e3d0401210ee1fd645b09ee0736079f742
Anish <technoflask03@gmail.com>
2022-09-14 22:54:28 +0530

    Updates

---
+++ precache-manifest.7b7f4585a981498294d4a05be328b2fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "724640aa825eedd1c092",
+    "url": "/First-React-App/static/css/main.ab8b8245.chunk.css"
+  },
+  {
+    "revision": "724640aa825eedd1c092",
+    "url": "/First-React-App/static/js/main.724640aa.chunk.js"
+  },
+  {
+    "revision": "745ee6c416dc6fee43ce",
+    "url": "/First-React-App/static/js/1.745ee6c4.chunk.js"
+  },
+  {
+    "revision": "0cb6f5346b33019beb54",
+    "url": "/First-React-App/static/js/2.0cb6f534.chunk.js"
+  },
+  {
+    "revision": "747f66c739bd91199ac0",
+    "url": "/First-React-App/static/js/runtime~main.747f66c7.js"
+  },
+  {
+    "revision": "deab5f9d088759abe3366a614a420089",
+    "url": "/First-React-App/index.html"
+  }
+];
